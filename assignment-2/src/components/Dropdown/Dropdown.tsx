8762dcb700fd01cf2cc9c5ee09227c778b19a419
Nandeesh V <131158894+nandeesh-cdw@users.noreply.github.com>
2024-04-24 10:49:39 +0530

    caret down

--- assignment-2/src/components/Dropdown/Dropdown.tsx
+++ assignment-2/src/components/Dropdown/Dropdown.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types';
 import { MODELS } from '../../models/Model';
 import styles from './Dropdown.module.scss';
-
+import caret from '../../assets/caret-down-solid.svg';
 import { useState } from 'react';
 function Dropdown({ options, onValueSelected, placeholder , value, isFormInput=false}: MODELS["DROPDOWNPROPS"]) {
 
 
   return (
+        <>
+        {/* <img */}
+        {/* <img src={caret} /> */}
         <select className={`${styles.dropdown} ${isFormInput? styles.form_input : ""}`} value={value} onChange={(event)=>onValueSelected(event.target.value)}>
+            <img src={caret} />
             <option value="" disabled={value !== ''}>{placeholder}</option>
             {options.map(option => (
                 <option key={option} value={option}>
@@ -16,6 +20,8 @@ function Dropdown({ options, onValueSelected, placeholder , value, isFormInput=f
                 </option>
             ))}
         </select>
+        </>
+        
   )
 }  
 
